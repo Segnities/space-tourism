23177879bb40fbd6e59805463e18c9142e9fc88a
Gadoro Zasandalio <70950389+Segnities@users.noreply.github.com>
2022-12-21 10:25:11 +0200

    prototype of link active class

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { headerLinks } from "../../../router";
 
 import Logotype from "./assets/img/logo.svg";
+import BurgerMenu from "./assets/img/icon-hamburger.svg";
+import CloseBurgerMenu from "./assets/img/icon-close.svg";
 
 import styles from "./assets/css/Header.module.css";
-
+import { useResizeObserver } from "../../../hooks/useResizeObserver";
 
 function Header() {
+  const clientWidth = useResizeObserver();
+  const location = useLocation();
+  const path = location.pathname;
+
   return (
     <header className={styles["header"]}>
       <div className={styles["logo"]}>
@@ -23,7 +29,11 @@ function Header() {
         <ul>
           {headerLinks.map((link, idx) => (
             <li key={link.id}>
-              <Link key={link.id} to={link.to}>
+              <Link
+                key={link.id}
+                to={link.to}
+                className={path === link.to ? styles["active"] : ""}
+              >
                 <b>0{idx}</b> <span>{link.title.toUpperCase()}</span>
               </Link>
             </li>
